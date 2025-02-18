30e8f17982b8b1d5a6a3c1865bbbf81e511fb02a
Normanas <normanas.zem5@gmail.com>
2025-02-18 14:17:17 +0200

    1

--- test.js
+++ test.js
@@ -3,15 +3,13 @@
 // @namespace   Violentmonkey Scripts
 // @match       *://sakiai.lt/vald/*
 // @grant       none
-// @version     1.2
+// @version     1.3
 // @author      -
-// @description 2/18/2025, 1:56:43 PM
+// @description 2/18/2025, 2:15:43 PM
 // @updateURL https://raw.githubusercontent.com/normanaszemaitis/tampermonkey-update-test-1/refs/heads/main/test.js
 // @downloadURL https://raw.githubusercontent.com/normanaszemaitis/tampermonkey-update-test-1/refs/heads/main/test.js
 // ==/UserScript==
 
 
 alert("test 1!");
-alert("test 2!");
-alert("test 3!");
 
